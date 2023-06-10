ea64609e49b65f4dd73efe5ce09bdf11f9d01557
Jose Jaime Come <josejaimecome@gmail.com>
2023-06-10 21:39:56 +0200

    HTML Tags and Django blocks for Product Categories added

--- e_commerce_django/urls.py
+++ e_commerce_django/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from contents import views
 
 urlpatterns = [
+    path('', views.Home, name="home"), 
     path('admin/', admin.site.urls),
 ]
