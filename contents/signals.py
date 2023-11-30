50a7121b3af61bae2b47c28205bd9f7fa241524d
Jose Jaime Come <josejaimecome@gmail.com>
2023-11-30 19:39:44 +0200

    Signals added

---
+++ contents/signals.py
@@ -0,0 +1,26 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.contrib.auth.models import User
+from contents.models import (
+    ProdCategory,
+    Product,
+    Cart,
+)
+
+
+@receiver(post_save, sender=ProdCategory)
+def user_created(sender, instance, created, **kwargs):
+   if created:
+      print('New Category of Products added:', instance.category)
+
+
+@receiver(post_save, sender=Product)
+def user_created(sender, instance, created, **kwargs):
+   if created:
+      print('New Product created:', instance.product)
+
+
+@receiver(post_save, sender=Cart)
+def user_created(sender, instance, created, **kwargs):
+   if created:
+      print('New Product in Cart added:', instance.article)
